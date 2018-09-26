0979e45775761ca7b68a30c7dc6b012ebace7519
gregvx <32096447+gregvx@users.noreply.github.com>
2018-09-26 05:06:26 -0600

    added in timer countdown functionality.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,16 +19,17 @@ $('#answer5').click(function() {
 });
 
 //global variables
-var gamePlayStarted = false;
-var wins= 0;
-var losses = 0;
-var wallet = 0;
-var target = 0;
-var gemValues = [0,0,0,0];
+var hits = 0;
+var misses = 0;
+var currentIndex = 0;
+var q1 = ["Question 1", "answer 1", ["false1", "false2", "false3", "false4"]];
+var q2 = ["Question 2", "answer 2", ["false2.1", "false2.2", "false3.2", "false4.2"]];
+var questions = [q1, q2];
 function startGame() {
-    alert("time to start game.");
+    // alert("time to start game.");
     document.getElementById('openingSplash').style.display = "none";
     document.getElementById('runningGameBox').style.display = "inline-block";
+    displayQuestion();
 }
 //this main function gets called when user clicks on an answer. a paramater x is passed
 // in corresponding to the index of the answer clicked.
@@ -36,3 +37,48 @@ function answerClicked(x) {
     alert("you clicked answer of index " + x);
 }
 
+//this funtion gets called every time a new question needs to be displayed
+//it looks at the global variables to determine what to put on screen
+function displayQuestion() {
+    var q = questions[currentIndex][0];
+    $('#questionField').html("<p>" + q + "</p>");
+    var answers = [];
+    answers.push(questions[currentIndex][1]);
+    for (var i = 0; i<4; i++) {
+        answers.push(questions[currentIndex][2][i])
+    }
+    
+    //now answers is an array of five strings. The first string is the correct answer,
+    //the next four strings are wrong answers. Need to shuffle the answers
+    answers = shuffleAnswers(answers);
+    
+    //now, prepare the html for the choices to display
+    answerChoicesHtml = "";
+    for (var j = 0; j<5; j++) {
+        answerChoicesHtml += ("<p>" + answers[j] + "</p>");
+    }
+    //now put that markup into the view
+    $('#choiceField').html(answerChoicesHtml);
+
+    //now, start a countdown timer
+    startTimer();
+}
+
+//this function takes an array of five strings and shuffles the order of the strings
+function shuffleAnswers(answers) {
+    //TODO write logic to shuffle the order...
+    return answers;
+}
+
+//this function starts a countdown and updates the view every second show the time remaining
+function startTimer() {
+    var t = 5;
+    var timer = setInterval(function(){
+        $('#timerClock').html("<p>Time remaining: " + t + "s</p>");
+        t--;
+        if (t === 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+    
+}
