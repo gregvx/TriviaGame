cd7b1583f401890262c769c98d89b82e659f2e19
gregvx <32096447+gregvx@users.noreply.github.com>
2018-09-26 03:56:11 -0600

    added splash screen with start button.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,38 @@
-//alert("your script is firing");
+//event handlers
+$('#startButton').click(function() {
+    startGame();
+});
+$('#answer1').click(function() {
+    answerClicked(0);
+});
+$('#answer2').click(function() {
+    answerClicked(1);
+});
+$('#answer3').click(function() {
+    ganswerClicked(2);
+});
+$('#answer4').click(function() {
+    answerClicked(3);
+});
+$('#answer5').click(function() {
+    answerClicked(4);
+});
+
+//global variables
+var gamePlayStarted = false;
+var wins= 0;
+var losses = 0;
+var wallet = 0;
+var target = 0;
+var gemValues = [0,0,0,0];
+function startGame() {
+    alert("time to start game.");
+    document.getElementById('openingSplash').style.display = "none";
+    document.getElementById('runningGameBox').style.display = "inline-block";
+}
+//this main function gets called when user clicks on an answer. a paramater x is passed
+// in corresponding to the index of the answer clicked.
+function answerClicked(x) {
+    alert("you clicked answer of index " + x);
+}
+
